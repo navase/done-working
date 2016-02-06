6a04b3dacac31792766bb2f737498cf98bbf125f
navase <margot.sauter@gmail.com>
2016-02-06 15:53:41 +0100

    Time(s) and Minute(s)

--- app/controllers/durations_controller.rb
+++ app/controllers/durations_controller.rb
@@ -6,7 +6,7 @@ class DurationsController < ApplicationController
   def create
     @time_off = Time.new(params[:time_off_hash][:year], params[:time_off_hash][:month], params[:time_off_hash][:day], params[:time_off_hash][:hour], params[:time_off_hash][:minute])
     @time_now = Time.new
-    @time_difference = Time.at(@time_off - @time_now)
+    @time_difference = Time.at(@time_off - @time_now -60*60)
     #@time_difference2 = TimeDifference.between(@time_off, @time_now).in_minutes
   end
 end
